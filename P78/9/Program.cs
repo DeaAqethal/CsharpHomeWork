5743a190bb4e3efec1f547893e008aa038d170fd
DeaAqethal <43109983+DeaAqethal@users.noreply.github.com>
2018-09-19 10:45:24 +0800

    Add files via upload

---
+++ P78/9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            for (int i = 2; i <= n; i++)
+            {
+                int j = 2; while (i % j != 0) {
+                    j++;
+                }
+                if (j == i) {
+                    Console.WriteLine(i + " ");
+                }
+            }
+            Console.WriteLine("结束");
+        }
+    }
+}
